8f12c75be9d7e26befc083d9c2c05f34e38691a6
Dick van den Brink <d_vandenbrink@outlook.com>
2015-01-18 19:56:34 +0100

    generate definition file as an external module

--- Scripts/generate-protocol-interfaces.ts
+++ Scripts/generate-protocol-interfaces.ts
@@ -1,59 +1,78 @@
 ﻿import fs = require("fs");
+import path = require("path");
 var protocol = require("../protocol.json");
 
 function indent(level: number) {
     return Array((level * 4) + 1).join(" ")
 }
 
-
 var domains = protocol.domains;
 var chromeDomains = "";
 
 for (var i = 0; i < domains.length; i++) {
-    chromeDomains += indent(2) + domains[i].domain + ": I" + domains[i].domain + ";\r\n";
+    chromeDomains += `${indent(2)}${domains[i].domain}: I${domains[i].domain};\r\n`;
 }
 
-var interfaces = "";
+var domainInterfaces = "";
+var methodParameterInterfaces = "";
 for (var i = 0; i < domains.length; i++) {
     emitInterface(domains[i]);
 }
 
 function emitInterface(domain: any) {
-    interfaces += indent(1) + "interface I" + domain.domain + " {\r\n"
+    domainInterfaces += `${indent(1)}interface I${domain.domain} {\r\n`;
     var commands: any[] = domain.commands;
     if (commands && commands.length > 0) {
         for (var i = 0; i < commands.length; i++) {
             emitCommand(commands[i]);
         }
     }
-    interfaces += indent(1) + "}\r\n";
+    domainInterfaces += indent(1) + "}\r\n";
 }
 
 function emitCommand(command: any) {
     if (command.description) {
-        interfaces += indent(2) + " /**\r\n";
-        interfaces += indent(2) + " * " + command.description + "\r\n";
-        interfaces += indent(2) + " */\r\n";
+        domainInterfaces += `${indent(2)} /**\r\n`;
+        domainInterfaces += `${indent(2)} * ${command.description}\r\n`;
+        domainInterfaces += `${indent(2)} */\r\n`;
     }
-    interfaces += indent(2) + command.name;
-    interfaces += "(";
+    domainInterfaces += indent(2) + command.name;
+    domainInterfaces += "(";
     var parameters: any[] = command.parameters;
     if (parameters && parameters.length > 0) {
-        for (var j = 0; j < parameters.length; j++) {
-            interfaces += parameters[j].name;
-            if (j < parameters.length - 1) {
-                interfaces += ", ";
-            }
-        }
+        var name = `I${command.name}Params`.replace(/I(.)/, (val) => val.toUpperCase());
+        domainInterfaces += `params: ${name}`;
+
+        emitParameterInterface(name, parameters);
+        domainInterfaces += ", ";
     }
-    interfaces += ");\r\n";
+    domainInterfaces += "cb: Function";
+    domainInterfaces += ");\r\n";
 }
 
-if (!fs.existsSync("bin")) {
-    fs.mkdirSync("bin");
+function emitParameterInterface(name: string, parameters: any[]) {
+    methodParameterInterfaces += `${indent(1)}interface ${name} {\r\n`;
+    for (var j = 0; j < parameters.length; j++) {
+        methodParameterInterfaces += `${indent(2)}${parameters[j].name};\r\n`;
+    }
+    methodParameterInterfaces += `${indent(1)}}\r\n`;
 }
 
-var maindts = fs.readFileSync("../main.d.ts", "utf8");
-maindts = maindts.replace(/(class ChromeDebugger (?:.+) {)([\s\S]+?)([ ]+)}/g, "$1$2" + chromeDomains + "$3}\r\n" + interfaces);
+var maindts = fs.readFileSync(path.join(__dirname, "../main.d.ts"), "utf8");
 
-fs.writeFileSync("../chrome-interfaces.d.ts", maindts);
+var importRegex = /import (?:.+;)/g;
+var imports = "";
+
+var match: RegExpExecArray;
+while ((match = importRegex.exec(maindts)) != null) {
+    imports += indent(1) + match[0] + "\r\n";
+}
+
+maindts = maindts.replace(/import (?:.+;)/g, "")
+    .replace(
+        /(class ChromeDebugger (?:.+) {)([\s\S]+?)([ ]+)}/g,
+        `$1$2${chromeDomains}$3}\r\n${domainInterfaces}${methodParameterInterfaces}`
+    ).replace(/declare module Chrome {/, "declare module \"chrome-debug-protocol\" {\r\n" + imports)
+    .replace("export = Chrome;", "");
+
+fs.writeFileSync(path.join(__dirname, "chrome-debug-protocol/chrome-debug-protocol.d.ts"), maindts);
