8a8f116a1f9252d37fc0f7b507dd0a7095765d03
Dick van den Brink <d_vandenbrink@outlook.com>
2015-01-19 00:04:29 +0100

    Implemented method parameters

--- main.ts
+++ main.ts
@@ -57,7 +57,7 @@ module Chrome {
             this.ws.close();
         }
 
-        public send(method: string, params: any[], callback: Function) {
+        public send(method: string, params: any, callback: Function) {
             if (this.ws.readyState == WebSocket.CONNECTING) {
                 this.ws.on("open",() => {
                     this.sendInternal(method, params, callback);
@@ -67,10 +67,9 @@ module Chrome {
             }
         }
 
-        private sendInternal(method: string, params: any[], callback: Function) {
+        private sendInternal(method: string, params: any, callback: Function) {
             console.log("Send command: " + method);
-            // TODO: parameters
-            this.ws.send(JSON.stringify({ method: method, id: this.callbackId }));
+            this.ws.send(JSON.stringify({ method, params, id: this.callbackId }));
             this.callbacks[this.callbackId] = callback;
             this.callbackId++;
         }
@@ -82,7 +81,11 @@ module Chrome {
                 // When an id is present, this means it is the return value from a method
                 var cb = this.callbacks[obj.id];
                 if (cb) {
-                    cb();
+                    if (obj.error) {
+                        cb(null, obj.error);
+                    } else {
+                        cb(obj.result, null);
+                    }
                     delete this.callbacks[this.callbackId];
                 }
             } else {
@@ -109,11 +112,13 @@ module Chrome {
         }
 
         private implementCommand(domain: any, object: Object, command: any) {
-            object[command.name] = () => {
-                var args: any[] = Array.prototype.slice.call(arguments);
+            object[command.name] = (args: Object) => {
                 var callback: Function;
-                if (args.length > 0 && typeof args[args.length - 1] == "function") {
-                    callback = args.splice(args.length - 1)[0];
+                if (arguments.length == 1 && typeof arguments[0] == "function") {
+                    callback = arguments[0];
+                    args = null;
+                } else {
+                    callback = arguments[1];
                 }
                 this.send(`${domain.domain}.${command.name}`, args, callback);
             };
