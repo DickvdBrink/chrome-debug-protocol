ff797ffb800a5893a3f97a351f668ef261837483
Dick van den Brink <d_vandenbrink@outlook.com>
2015-01-18 15:34:30 +0100

    Changes to interface generation Not yet usable though

--- Scripts/generate-protocol-interfaces.ts
+++ Scripts/generate-protocol-interfaces.ts
@@ -4,48 +4,50 @@ var protocol = require("../protocol.json");
 var domains = protocol.domains;
 var str = "";
 
-str += "declare module Chrome {\r\n";
-str += "\tinterface ChromeDomains {\r\n";
 for (var i = 0; i < domains.length; i++) {
-    str += "\t\t" + domains[i].domain + ": I" + domains[i].domain + ";\r\n";
-}
-str += "\t}\r\n";
-str += "}\r\n";
-
-
-for (var i = 0; i < domains.length; i++) {
-    emitInterface(domains[i]);
+    str += "\t\t" + domains[i].domain + ": any;\r\n";
 }
 
-function emitInterface(domain: any) {
-    str += "interface I" + domain.domain + " {\r\n"
-    var commands: any[] = domain.commands;
-    if (commands && commands.length > 0) {
-        for (var i = 0; i < commands.length; i++) {
-            emitCommand(commands[i]);
-        }
-    }
-    str += "}\r\n";
-}
+var maindts = fs.readFileSync("../main.d.ts", "utf8");
+str = maindts.replace(/(class ChromeDebugger (?:.+) {)([\s\S]+?)([ ]+)}/g, "$1$2" + str + "$3}")
 
-function emitCommand(command: any) {
-    if (command.description) {
-        str += "    /**\r\n";
-        str += "     * " + command.description + "\r\n";
-        str += "     */\r\n";
-    }
-    str += "    " + command.name;
-    str += "(";
-    var parameters: any[] = command.parameters;
-    if (parameters && parameters.length > 0) {
-        for (var j = 0; j < parameters.length; j++) {
-            str += parameters[j].name;
-            if (j < parameters.length - 1) {
-                str += ",";
-            }
-        }
-    }
-    str += ");\r\n";
+//for (var i = 0; i < domains.length; i++) {
+//    emitInterface(domains[i]);
+//}
+
+//function emitInterface(domain: any) {
+//    str += "interface I" + domain.domain + " {\r\n"
+//    var commands: any[] = domain.commands;
+//    if (commands && commands.length > 0) {
+//        for (var i = 0; i < commands.length; i++) {
+//            emitCommand(commands[i]);
+//        }
+//    }
+//    str += "}\r\n";
+//}
+
+//function emitCommand(command: any) {
+//    if (command.description) {
+//        str += "    /**\r\n";
+//        str += "     * " + command.description + "\r\n";
+//        str += "     */\r\n";
+//    }
+//    str += "    " + command.name;
+//    str += "(";
+//    var parameters: any[] = command.parameters;
+//    if (parameters && parameters.length > 0) {
+//        for (var j = 0; j < parameters.length; j++) {
+//            str += parameters[j].name;
+//            if (j < parameters.length - 1) {
+//                str += ",";
+//            }
+//        }
+//    }
+//    str += ");\r\n";
+//}
+
+if (!fs.existsSync("bin")) {
+    fs.mkdirSync("bin");
 }
 
 fs.writeFileSync("../chrome-interfaces.d.ts", str);
