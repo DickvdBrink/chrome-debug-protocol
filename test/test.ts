3ac47a1508ac16cf4d7b61265fd485aafd76ffb0
Dick van den Brink <d_vandenbrink@outlook.com>
2015-01-19 20:49:30 +0100

    Added example

---
+++ test/test.ts
@@ -0,0 +1,15 @@
+﻿import chrome = require("chrome-debug-protocol");
+import main = require("../main");
+
+main.getTabs("http://localhost:9444/json",(tabs) => {
+    var chromeTab: chrome.ChromeDebugger = <any>main.createDebugger(tabs[0]);
+    chromeTab.Console.enable(() => {
+        console.log("Enabled");
+    });
+    chromeTab.Console.on("messageAdded",(m: chrome.Console.IMessageAddedEvent) => {
+        console.log(m.message.source);
+    });
+    chromeTab.on("Console.messageAdded",(m: chrome.Console.IMessageAddedEvent) => {
+        console.log(m.message.source);
+    });
+});
