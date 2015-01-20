d6bd6c58c8a3dc77c02b30245db93408220b82dc
Dick van den Brink <d_vandenbrink@outlook.com>
2015-01-20 21:35:50 +0100

    Allow building using Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+﻿module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        ts: {
+            default: {
+                src: ["**/*.ts", "!node_modules/**/*.ts"],
+                options: {
+                    compiler: "./node_modules/typescript/bin/tsc",
+                    module: "commonjs",
+                    target: "es5",
+                    declaration: true
+                }
+            }
+        },
+        execute: {
+            generate_definition: {
+                src: ['Scripts/generate-protocol-interfaces.js']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-ts");
+    grunt.loadNpmTasks("grunt-execute");
+    grunt.registerTask("default", ["ts", "execute:generate_definition"]);
+};
