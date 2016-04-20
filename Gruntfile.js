882a0e39eecbade1f89eec165af12769d2a32a1e
Dick van den Brink <d_vandenbrink@outlook.com>
2016-04-20 20:50:33 +0200

    Upgrade grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,19 +3,16 @@
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         ts: {
-            default: {
-                src: ["**/*.ts", "!node_modules/**/*.ts"],
-                options: {
-                    compiler: "./node_modules/typescript/bin/tsc",
-                    module: "commonjs",
-                    target: "es5",
-                    declaration: true
-                }
+            scripts: {
+                tsconfig: './scripts/tsconfig.json'
+            },
+            src: {
+                tsconfig: './src/tsconfig.json'
             }
         },
         execute: {
             generate_definition: {
-                src: ['Scripts/generate-protocol-interfaces.js']
+                src: ['scripts/generate-protocol-interfaces.js']
             }
         },
         connect: {
